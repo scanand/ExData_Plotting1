fc7b331615dfc98565653a33ab5f64ec295c4720
SCAnand <dev@anand-pardhi.com>
2014-07-14 09:25:33 +1000

    last commit

--- source/plot4.R
+++ source/plot4.R
@@ -24,7 +24,7 @@ par("mar")
 plot(extractedData$day,extractedData$Global_active_power,type="l",xlab="",ylab="Global Active Power" )
 plot(extractedData$day,extractedData$Voltage,type="l",ylab="Voltage" ,xlab="datetime")
 
-plot(extractedData$day,extractedData$Sub_metering_1,type="n",)
+plot(extractedData$day,extractedData$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
 lines(extractedData$day,extractedData$Sub_metering_1,col="black")
 lines(extractedData$day,extractedData$Sub_metering_2,col="red")
 lines(extractedData$day,extractedData$Sub_metering_3,col="blue")
@@ -35,7 +35,7 @@ legend("topright",
        lty=c(1,1,1),                    # Line type  
        lwd=c(1,1,1)                    # Line width  
 )
-plot(extractedData$day,extractedData$Global_reactive_power,type="l",xlab="datetime" )
+plot(extractedData$day,extractedData$Global_reactive_power,type="l",xlab="datetime" ,ylab="Global_reactive_power")
 
 dev.print(png,file ="plot4.png", width = 480, height = 480)
 dev.off()
